e72bd69d4abe075a86253c8b15f27adcc7aa0640
dannd1 dannd1 <dannd1@viettel.com.vn>
2025-05-20 10:11:55 +0700

    add main

---
+++ spring-video/src/main/java/com/streaming/video/StreamVideoApplication.java
@@ -0,0 +1,11 @@
+package com.streaming.video;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamVideoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamVideoApplication.class, args);
+    }
+} 
